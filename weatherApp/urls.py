1cbf40ef78aa931ac96fd5506a338c1eb0ac3979
Matt Weems <weemsmatthewj@gmail.com>
2017-06-21 19:48:39 -0500

    initial commit

---
+++ weatherApp/urls.py
@@ -0,0 +1,8 @@
+
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^weather/', include('weather.urls')),
+]
