7daeb913813d8c3cc0288ffbebfd4e624f1ccf13
Matt Weems <weemsmatthewj@gmail.com>
2017-06-21 20:27:01 -0500

    wsgi adjustment

--- weatherApp/wsgi.py
+++ weatherApp/wsgi.py
@@ -13,5 +13,6 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weatherApp.settings")
 
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
