98bf74d565ab37aa1bb3a74cd68099e216a396c6
Matt Weems <weemsmatthewj@gmail.com>
2017-06-22 11:22:57 -0500

    add basic header with button and login view

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-	return render(request, 'index.html')
+	user = 'Matt Weems'
+	return render(request, 'index.html', {'user': user})
+
+def login(request):
+	return render(request, 'login.html')
--- weatherApp/urls.py
+++ weatherApp/urls.py
@@ -1,8 +1,10 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from weather import views 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('weather.urls')),
+    url(r'^$', views.index, name='index'),
+	url(r'^login', views.login, name='login' ),
 ]
