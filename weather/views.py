11813abfe8299c454349cfc99522a50adb6c59a7
Matt Weems <weemsmatthewj@gmail.com>
2017-06-22 19:56:23 -0500

    different syntax

--- weather/views.py
+++ weather/views.py
@@ -9,7 +9,7 @@ def index(request):
 	if request.method == 'POST':
 		city = request.POST.get('city')
 		data = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + '&mode=json&units=imperial&APPID=9f592b7a51c96031cae6cb887d0594c4')
-		content = data['content'].json()
+		content = json.loads(data.content.text)
 		today = {'city': content['name'], 'temp': content['main']['temp'], 'temp_max': content['main']['temp_max'],
 				'temp_min': content['main']['temp_min'], 'description': content['weather'][0]['description']}
 		return render(request, 'index.html', {'user': user, 'today': today})
