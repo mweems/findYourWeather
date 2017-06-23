2eb3f86e812c67d1348cef172f4f6fb65b4a2641
Matt Weems <weemsmatthewj@gmail.com>
2017-06-22 19:45:33 -0500

    add api call to get current days weather

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,18 @@
+import requests
+import json
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def index(request):
 	user = 'Matt Weems'
+	if request.method == 'POST':
+		city = request.POST.get('city')
+		data = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + '&units=imperial&APPID=9f592b7a51c96031cae6cb887d0594c4')
+		content = json.loads(data.content)
+		today = {'city': content['name'], 'temp': content['main']['temp'], 'temp_max': content['main']['temp_max'],
+				'temp_min': content['main']['temp_min'], 'description': content['weather'][0]['description']}
+		return render(request, 'index.html', {'user': user, 'today': today})
 	return render(request, 'index.html', {'user': user})
 
 def login(request):
