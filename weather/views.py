2521d2e4abf07569c86d195a47b36ebd17602ded
Matt Weems <weemsmatthewj@gmail.com>
2017-06-24 11:02:39 -0500

    add try except for when no results are returned

--- weather/views.py
+++ weather/views.py
@@ -11,10 +11,33 @@ def index(request):
 		city = request.POST.get('city')
 		daily = getDailyForecast(city)
 		forecast = getWeeklyForecast(city)
-		
+
+		return render(request, 'index.html', {
+			'user': user, 
+			'today': daily, 
+			'forecast': forecast})
+	return render(request, 'index.html', {'user': user})
+
+def login(request):
+	return render(request, 'login.html')
+
+def getDailyForecast(city):
+	try:
+		daily = requests.get("http://api.openweathermap.org/data/2.5/weather?q="
+			 	+ city + '&mode=json&units=imperial&APPID=9f592b7a51c96031cae6cb887d0594c4')
+		daily = daily.json()
 		today = {'city': daily['name'], 'temp': int(daily['main']['temp']),
-				 'temp_max': int(daily['main']['temp_max']),'temp_min': int(daily['main']['temp_min']),
-				  'description': daily['weather'][0]['main']}
+					'temp_max': int(daily['main']['temp_max']),'temp_min': int(daily['main']['temp_min']),
+					'description': daily['weather'][0]['main']}
+	except:
+		today = {'error': 'The City ' + city + ' could not be found. Please check the spelling and try again'}
+	return today
+
+def getWeeklyForecast(city):
+	try:
+		forecast = requests.get("http://api.openweathermap.org/data/2.5/forecast?q=" 
+				+ city + '&mode=json&units=imperial&APPID=9f592b7a51c96031cae6cb887d0594c4')
+		forecast = forecast.json()
 		forecast_list = []
 		for data in forecast['list']:
 			rep = {
@@ -26,21 +49,6 @@ def index(request):
 				'main': data['weather'][0]['main']
 			}
 			forecast_list.append(rep)
-		return render(request, 'index.html', {
-			'user': user, 
-			'today': today, 
-			'forecast': forecast_list})
-	return render(request, 'index.html', {'user': user})
-
-def login(request):
-	return render(request, 'login.html')
-
-def getDailyForecast(city):
-	daily = requests.get("http://api.openweathermap.org/data/2.5/weather?q="
-		 	+ city + '&mode=json&units=imperial&APPID=9f592b7a51c96031cae6cb887d0594c4')
-	return daily.json()
-
-def getWeeklyForecast(city):
-	forecast = requests.get("http://api.openweathermap.org/data/2.5/forecast?q=" 
-			+ city + '&mode=json&units=imperial&APPID=9f592b7a51c96031cae6cb887d0594c4')
-	return forecast.json()
+	except:
+		forecast_list = {'error': True}
+	return forecast_list
