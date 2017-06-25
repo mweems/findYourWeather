5eaf9f1c9c5167fdeb620552cd088d61bc2f7a49
Matt Weems <weemsmatthewj@gmail.com>
2017-06-25 18:25:46 -0500

    again with collect static

--- weatherApp/urls.py
+++ weatherApp/urls.py
@@ -2,7 +2,8 @@
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
-from weather import views 
+from weather import views
+from weatherApp import settings 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -12,5 +13,6 @@ urlpatterns = [
     url(r'^set_current/(?P<city>([\w\ ]+))/$', views.set_current, name='set_current'),
 	url(r'^signup', views.signup, name='signup' ),
 	url(r'^login/$', auth_views.login, {'template_name': 'login.html'},name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout')
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
